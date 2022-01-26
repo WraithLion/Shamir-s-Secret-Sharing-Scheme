3909912e8a6e12303466158778d9706e91d33300
JohnPavilon <75518367+JohnPavilon@users.noreply.github.com>
2022-01-25 21:51:15 -0600

    Delete a.py

--- Esquema Secreto Compartido de Shamir (SSSS)/a.py
+++
@@ -1,8 +0,0 @@
-import cv2
-path = r'C:\Users\jonqt\Music\Python\Esteganografía\RevealShowApp\Imágenes\Ulthwe.png'
-# Using cv2.imread() method
-# Using 0 to read image in grayscale mode
-img = cv2.imread('Ulthwe.png')
-  
-# Displaying the image
-cv2.imshow('image', img)
